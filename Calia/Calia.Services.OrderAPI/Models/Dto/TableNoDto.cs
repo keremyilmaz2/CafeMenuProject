43c84288e6c69fa05114f68cd784d2adcb82ccf2
kerem yılmaz <keremiremsu14@gmail.com>
2025-03-07 23:37:01 +0300

    shoppingcart_api

--- Calia/Calia.Services.OrderAPI/Models/Dto/TableNoDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Calia.Services.OrderAPI.Models.Dto
-{
-    public class TableNoDto
-    {
-        public int Id { get; set; }
-        public string MasaNo { get; set; }
-        public bool IsOccupied { get; set; }
-    }
-}
