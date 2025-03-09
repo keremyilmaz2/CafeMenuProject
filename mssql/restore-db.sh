43ba2ae568ca2c43c0ed0c487f98ddf903f53e3e
kerem yılmaz <keremiremsu14@gmail.com>
2025-03-09 23:37:20 +0300

    changes

---
+++ mssql/restore-db.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "MSSQL'in başlaması bekleniyor..."
+until /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'Keremkerem11!' -Q "SELECT 1" &> /dev/null
+do
+  sleep 5
+  echo "Bekleniyor..."
+done
+
+echo "MSSQL Başladı! Geri yükleme başlıyor..."
+
+for db in Auth Category Order Product ShoppingCart Stock
+do
+  /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'Keremkerem11!' -Q "
+  RESTORE DATABASE ${db}DB FROM DISK = '/var/opt/mssql/backups/Calia_${db}API.bak' 
+  WITH MOVE '${db}DB' TO '/var/opt/mssql/data/${db}DB.mdf', 
+       MOVE '${db}DB_Log' TO '/var/opt/mssql/data/${db}DB.ldf', REPLACE;"
+done
+
+echo "Tüm veritabanları başarıyla geri yüklendi!"
