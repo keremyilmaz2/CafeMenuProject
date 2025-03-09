0dd72ec02de33517edff12aa7f3db07cf5b0a5b3
kerem yılmaz <keremiremsu14@gmail.com>
2025-03-10 00:28:10 +0300

    restore

--- mssql/restore-db.sh
+++ mssql/restore-db.sh
@@ -13,8 +13,8 @@ for db in Auth Category Order Product ShoppingCart Stock
 do
   /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'Keremkerem11!' -Q "
   RESTORE DATABASE ${db}DB FROM DISK = '/var/opt/mssql/backups/Calia_${db}API.bak' 
-  WITH MOVE '${db}DB' TO '/var/opt/mssql/data/${db}DB.mdf', 
-       MOVE '${db}DB_Log' TO '/var/opt/mssql/data/${db}DB.ldf', REPLACE;"
+  WITH MOVE 'Calia_${db}API' TO '/var/opt/mssql/data/${db}DB.mdf', 
+       MOVE 'Calia_${db}API_log' TO '/var/opt/mssql/data/${db}DB.ldf', REPLACE;"
 done
 
 echo "Tüm veritabanları başarıyla geri yüklendi!"
